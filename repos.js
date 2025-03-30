0dc7eca6c36013062498f0600845dea8cc8eb4dd
vijitdua <82555472+vijitdua@users.noreply.github.com>
2025-03-29 17:17:19 -0700

    updated private repo info

--- repos.js
+++ repos.js
@@ -1,6 +1,6 @@
 export const REPOS = {
     repo1: "https://github.com/username/repo1.git",
     repo2: "https://github.com/username/repo2.git",
-    privateRepo: "https://<your_token>@github.com/username/private-repo.git"
+    privateRepo: "https://github.com/username/private-repo.git" //will automatically ask you for your github token once in the command line
 };
 
